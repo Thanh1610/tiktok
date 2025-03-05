827941dd68d51895c70f5a64e78ab3a2b78d443c
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-03-05 17:55:05 +0700

    13.Xây dựng Logic Header #2

--- src/components/AccountIem/index.jsx
+++ src/components/AccountIem/index.jsx
@@ -1,26 +1,23 @@
 import classNames from 'classnames/bind';
+import { Link } from 'react-router';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <img
-                className={cx('avatar')}
-                src="https://p9-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/461cb75d51cb95189b7a7325d99dc509~tplv-tiktokx-cropcenter:100:100.jpeg?dr=14579&x-expires=1741082400&x-signature=5M1z5nHNep5FSsjU7Lhd7K7VbOg%3D&t=4d5b0474&ps=13740610&shp=30310797&shcp=c1333099&idc=sg1"
-                alt="Hoa"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <img className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>realpewpew</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>Tạp Hóa PewPew</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -3,21 +3,23 @@ import images from '../../assets/images';
 import classNames from 'classnames';
 import styles from '../Image/';
 
-function Image({ src, alt, ...props }) {
+const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
     const [fallback, setFallback] = useState('');
+
     const handleError = () => {
-        setFallback(images.noImage);
+        setFallback(customFallback);
     };
 
     return (
         <img
-            className={classNames(styles.wrapper, classNames)}
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
             src={fallback || src}
             alt={alt}
             {...props}
             onError={handleError}
         />
     );
-}
+});
 
-export default forwardRef(Image);
+export default Image;
