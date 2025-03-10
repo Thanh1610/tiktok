89bacdb5fadf02a64e37bb362205ef04c89b8c57
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-03-10 15:38:24 +0700

    19.Xây dựng Ui sidebar #1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,12 +7,14 @@ import Following from '../pages/Following';
 import Profile from '../pages/Profile';
 import Upload from '../pages/Upload';
 import Search from '../pages/Search';
+import Live from '../pages/Live';
 
 //public routes
 export const publicRoutes = [
     { path: routesConfig.home, component: Home },
     { path: routesConfig.following, component: Following },
     { path: routesConfig.profile, component: Profile },
+    { path: routesConfig.live, component: Live },
     { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
     { path: routesConfig.search, component: Search, layout: null },
 ];
